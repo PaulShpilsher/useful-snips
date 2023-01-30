0f5a7157e73aa6ae63bc260a202921caa966f5c6
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-01-30 10:08:02 +0300

    add is jspon string

---
+++ javascript/value/is-json.js
@@ -0,0 +1,24 @@
+/**
+ * Is valid JSON string
+ * @param {*} json string to test
+ * @returns true/falss
+ */
+const isJSON = (json) => {
+  if (typeof json === "string") {
+    try {
+      JSON.parse(json);
+      return true;
+    } catch {}
+  }
+  return false;
+};
+
+console.log(isJSON('{"id": 1}'));               // true
+console.log(isJSON('[1,2,"foo", {"id": 1}]'));  // true
+
+console.log(isJSON("not a JSON string"));       // false
+console.log(isJSON());                          // false
+console.log(isJSON(null));                      // false
+console.log(isJSON(42));                        // false
+console.log(isJSON([77]));                      // false
+console.log(isJSON({ a: 2 }));                  // false
