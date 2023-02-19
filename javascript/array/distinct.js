74b5e8ee42f8b79ddc3aa7ca7639117a3092c717
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-02-19 19:47:50 +0300

    fix typo

--- javascript/array/distinct.js
+++ javascript/array/distinct.js
@@ -3,14 +3,14 @@
  *
  */
 
-const disinct = (arr) => [...new Set(arr)];
+const distinct = (arr) => [...new Set(arr)];
 
 const numbers = [1, 1, 2, 3, 1, 2, 3];
 
-console.log(disinct(numbers));
+console.log(distinct(numbers));
 // output: [ 1, 2, 3 ]
 
 const words = ["foo", "bar", "bar", "foo"];
 
-console.log(disinct(words));
+console.log(distinct(words));
 // output: [ 'foo', 'bar' ]
