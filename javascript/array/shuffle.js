e0c1dc3ba85b12219a63cbde0c239390a425087e
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-03-23 09:17:14 +0300

    refactor

---
+++ javascript/array/shuffle.js
@@ -0,0 +1,10 @@
+/**
+ * Shuffles arrray values
+ */
+const shuffle = (arr) => arr.sort(() => Math.random()-0.5);
+
+
+const src = [1,2,'three', 300, Math.PI, { foo: 'bar'}];
+const dest = shuffle(src);
+
+console.log(dest); // [ 1, 3.141592653589793, { foo: 'bar' }, 300, 'three', 2 ]
