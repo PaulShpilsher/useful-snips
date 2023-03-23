d0f948e9ce17f17a1df9eb1dbda11f7a9404fd40
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-03-23 09:18:33 +0300

    add comment

--- javascript/array/shuffle.js
+++ javascript/array/shuffle.js
@@ -7,4 +7,5 @@ const shuffle = (arr) => arr.sort(() => Math.random()-0.5);
 const src = [1,2,'three', 300, Math.PI, { foo: 'bar'}];
 const dest = shuffle(src);
 
-console.log(dest); // [ 1, 3.141592653589793, { foo: 'bar' }, 300, 'three', 2 ]
+// note: your results may vary )
+console.log(dest); // [ 1, 3.141592653589793, { foo: 'bar' }, 300, 'three', 2 ]   
